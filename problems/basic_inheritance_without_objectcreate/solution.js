ceb194350dc3a3792003afce7187ac35d2e4d0f2
Tim <secoif@gmail.com>
2013-10-03 19:10:02 +0800

    Create inheritance exercise with & without Object.create

---
+++ problems/basic_inheritance_without_objectcreate/solution.js
@@ -0,0 +1,15 @@
+module.exports = function(User) {
+  function BetterUser() {
+    User.apply(this, arguments)
+  }
+
+  function C() {}
+  C.prototype = User.prototype
+  BetterUser.prototype = new C()
+
+  BetterUser.prototype.toString = function() {
+    return '[BetterUser: '+this.name+' ('+ this.age +')]'
+  }
+
+  return BetterUser
+}
