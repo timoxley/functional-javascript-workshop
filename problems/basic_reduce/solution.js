c52a2c63da101a38ab5b0966d48bdc150cff9b8d
Tim Oxley <secoif@gmail.com>
2013-12-08 09:09:17 +1000

    Add more descriptive variable names + comments
    https://github.com/nodeschool/discussions/issues/101

--- problems/basic_reduce/solution.js
+++ problems/basic_reduce/solution.js
@@ -1,8 +1,8 @@
 function countWords(arr) {
-  return arr.reduce(function(prev, curr) {
-    prev[curr] = ++prev[curr] || 1
-    return prev
-  }, {})
+  return arr.reduce(function(countMap, word) {
+    countMap[word] = ++countMap[word] || 1 // increment or initialize to 1
+    return countMap
+  }, {}) // second argument to reduce initialises countMap to {}
 }
 
 module.exports = countWords
