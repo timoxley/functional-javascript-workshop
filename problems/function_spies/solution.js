0baead046deeaea1bd5527303ca6f6eb6b48fd8d
Tim Oxley <secoif@gmail.com>
2014-01-03 18:28:21 +0800

    Use call-time value of `this` in spy.
    Closes #39.

--- problems/function_spies/solution.js
+++ problems/function_spies/solution.js
@@ -1,12 +1,16 @@
 function Spy(target, method) {
   var originalFunction = target[method]
+
+  // use an object so we can pass by reference, not value
+  // i.e. we can return result, but update count from this scope
   var result = {
     count: 0
   }
 
+  // replace method with spy method
   target[method] = function() {
-    result.count++
-    return originalFunction.apply(target, arguments)
+    result.count++ // track function was called
+    return originalFunction.apply(this, arguments) // invoke original function
   }
 
   return result
