46e3c26cc4f23431fbedef33c7f1eb3e5751cc91
Tim Oxley <secoif@gmail.com>
2014-01-08 19:37:51 +0800

    Fix number of cycles typo
    #40

--- problems/blocking_event_loop/setup.js
+++ problems/blocking_event_loop/setup.js
@@ -2,28 +2,33 @@
 
 var input = require('../../input')
 
+
+
 module.exports = input().wrap(function(input, repeat) {
+
+  var count = 0
+  var CYCLES = 100000
+
+  function operation() {
+    for (var i = 0; i < 1000000; i++) {}  // burn some CPU cycles
+    count++ // count how many times this function was called
+  }
+
   console.log()
   console.log('the operation:')
   console.log(operation.toString())
   console.log()
-  console.log('Trying to repeat the operation 100000 times...')
+  console.log('Trying to repeat the operation %d times...', CYCLES)
   console.log('Press control+c to kill.')
   console.log()
-  var count = 0
-  function operation() {
-    // burn some cpu cycles
-    for (var i = 0; i < 1000000; i++) {}
-    // count how many times this function was called
-    count++
-  }
+
   var start = Date.now()
-  repeat(operation, 100000)
+  repeat(operation, CYCLES)
 
   setTimeout(function() {
     var end = Date.now()
     console.error('Performed %d operations.', count)
-    if (count === 10000) console.log('Fail! Should not have completed all operations!')
+    if (count === CYCLES) console.log('Fail! Should not have completed all operations!')
     // TODO calculate ideal ops per second?
     //if (count < 1000) console.log('Fail! Should have performed more operations!')
     if (end - start < 1500) console.log('Interrupted in approximately 1 second!')
