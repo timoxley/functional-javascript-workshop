208c437591b40ed0c9bc23603fcc22f3bd475b58
Christophe Porteneuve <tdd@tddsworld.com>
2015-03-15 19:31:49 +0100

    Migrated trampoline exercise

--- exercises/trampoline/exercise.js
+++ exercises/trampoline/exercise.js
@@ -1,13 +1,12 @@
 "use strict"
 
-var input = require('../../input')
+var runner = require('../../runner')
 
-module.exports = input().wrap(function(input, repeat) {
+module.exports = runner.custom(function(repeat) {
   console.log('repeating 100000 times')
   var count = 0
   repeat(function() {
     count++
   }, 100000)
   console.log('Sucessfully executed %d times.', count)
-})
-
+}).quiet()
