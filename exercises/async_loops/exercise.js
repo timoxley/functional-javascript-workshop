f7aa40608adac6d56006a2560719aac5e6243706
Christophe Porteneuve <tdd@tddsworld.com>
2015-03-16 11:33:31 +0100

    Migrated async-loops exercise

--- exercises/async_loops/exercise.js
+++ exercises/async_loops/exercise.js
@@ -1,27 +1,41 @@
 "use strict"
 
-var input = require('../../input')
-var lorem = require('lorem-ipsum')
+var deepEqual = require('deep-eql')
+var inspect = require('util').inspect
+var loremIpsum = require('lorem-ipsum')
+var runner = require('../../runner')
 
 function randomInt(min, max) {
-  return Math.floor((Math.random() * (max - min + 1)) + min)
+  return Math.floor((Math.random() * (max - min)) + min)
 }
 
-module.exports = input(new Array(randomInt(10, 20))
-.join(',')
-.split(',')
-.map(function() {
+// All deepEqual impls (assert, deep-eql…) seem to b0rk when multiple objects
+// in an array share the same `id` value (wtf?!), so we make sure they're unique.
+var userCount = randomInt(10, 20)
+var userIds = []
+while (userIds.length < userCount) {
+  var id = randomInt(0, 1000)
+  if (-1 !== userIds.indexOf(id)) continue
+    userIds.push(id)
+}
+
+var users = Array.apply(null, { length: userCount }).map(function() {
   return {
-    id: randomInt(0, 1000),
-    name: lorem().split(' ').slice(0, 2).map(function(word) {word[0] = word[0].toUpperCase(); return word;}).join(' ')
+    id: userIds.shift(),
+    name: loremIpsum().split(' ').slice(0, 2).map(function(word) {
+      word[0] = word[0].toUpperCase();
+      return word;
+    }).join(' ')
   }
-})).wrap(function(input, mod) {
+})
 
-  var assert = require('assert')
-  var inspect = require('util').inspect
+var fx
 
-  var users = input[0]
-  var ids = users.map(function(user) {return user.id})
+module.exports = runner.custom(function(f) {
+  fx = f
+}).wrapUp(function(callback) {
+  var self = this
+  var ids = users.map(function(user) {return user.id })
   var load = function(id, fn) {
     setTimeout(function() {
       var match = users.filter(function(user) {return user.id === id})
@@ -33,13 +47,18 @@ module.exports = input(new Array(randomInt(10, 20))
     clearTimeout(tooLong)
     console.log(submittedUsers)
 
-    assert.deepEqual(submittedUsers, users, 'expected: \n' + inspect(users) + '\n but got: \n'+ inspect(submittedUsers))
+    if (!deepEqual(submittedUsers, users)) {
+      self.emit('fail', 'expected: \n' + inspect(users) + '\n but got: \n'+ inspect(submittedUsers))
+      return callback(null, false)
+    }
 
     console.log('All %d users loaded!', submittedUsers.length)
+    callback(null, true)
   }
 
-  mod.call(mod, ids, load, done)
+  fx.call(fx, ids, load, done)
   var tooLong = setTimeout(function() {
-    throw new Error('Took too long!')
+    self.emit('fail', 'Took too long!')
+    callback(null, false)
   }, 1000)
-})
+}).quiet(users)
