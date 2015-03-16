dc13bb3927534c9b26703216e276a4a5661a5913
Christophe Porteneuve <tdd@tddsworld.com>
2015-03-16 11:44:23 +0100

    Migrated currying exercise

--- exercises/currying/exercise.js
+++ exercises/currying/exercise.js
@@ -1,32 +1,34 @@
 /**
  * Created by naor on 10/10/13.
+ * Migrated by tdd on 03/16/15.
  */
 "use strict";
 
-var input = require('../../input');
+var runner = require('../../runner')
 
-module.exports = input().wrap(function(input, curryN) {
-
+module.exports = runner.custom(function(curryN) {
   function add3(one, two, three) {
     return one + two + three
   }
-  console.log("var curryC = curryN(add3)")
-  console.log("var curryB = curryC(1)")
-  console.log("var curryA = curryB(2))")
+  // console.log("var curryC = curryN(add3)")
+  // console.log("var curryB = curryC(1)")
+  // console.log("var curryA = curryB(2))")
   var curryC = curryN(add3)
   var curryB = curryC(1)
   var curryA = curryB(2)
 
-  console.log("curryA(3) => ", curryA(3)) // => 6
-  console.log("curryA(10) =>", curryA(10)) // => 13
-  console.log("curryN(add3)(1)(2)(3) =>", curryN(add3)(1)(2)(3)) // => 6
-
+  var result = []
+  result.push("curryA(3) => " + curryA(3)) // => 6
+  result.push("curryA(10) => " + curryA(10)) // => 13
+  result.push("curryN(add3)(1)(2)(3) => " + curryN(add3)(1)(2)(3)) // => 6
 
   function strConcat(){
     var args = Array.prototype.slice.call(arguments);
     return Array.prototype.concat.apply([], args).join(" ");
   }
 
-  console.log("curryN(strConcat, 5)('This')('problem')('has')('been')('solved')) =>")
-  console.log(curryN(strConcat, 5)('This')('problem')('has')('been')('solved'));
-});
+  result.push(
+    "curryN(strConcat, 5)('This')('problem')('has')('been')('solved')) => " +
+    curryN(strConcat, 5)('This')('problem')('has')('been')('solved'))
+  return result
+}).hideInput()
