aacb4c963c005a5e241262ad3d792e380244df72
Julian Dreissig <jdreissig@paypal.com>
2015-06-18 10:13:33 +0200

    Fixed map-with-reduce solution to correctly process array entries
    Previous solution was flattening array results of the iterator.

--- exercises/implement_map_with_reduce/solution/solution.js
+++ exercises/implement_map_with_reduce/solution/solution.js
@@ -1,5 +1,6 @@
 module.exports = function map(arr, fn, thisArg) {
   return arr.reduce(function(acc, item, index, arr) {
-    return acc.concat(fn.call(thisArg, item, index, arr))
+    acc.push(fn.call(thisArg, item, index, arr))
+    return acc
   }, [])
 }
