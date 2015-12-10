5fb8ab4e74bd0ee908719317e52b692cec282b51
Dave Kerr <davek09@gmail.com>
2015-12-09 20:06:01 -0500

    update solution for trampoline problem

--- exercises/trampoline/solution/solution.js
+++ exercises/trampoline/solution/solution.js
@@ -1,19 +1,20 @@
 function repeat(operation, num) {
   return function() {
-    if (num <= 0) return
-    operation()
-    return repeat(operation, --num)
-  }
+    if (num <= 0) { return; }
+    operation();
+    return repeat(operation, --num);
+  };
 }
 
 function trampoline(fn) {
   while(fn && typeof fn === 'function') {
-    fn = fn()
+    return fn();
   }
 }
 
 module.exports = function(operation, num) {
-  trampoline(function() {
-    return repeat(operation, num)
-  })
-}
+  return trampoline(function() {
+    return repeat(operation, num);
+  });
+};
+
