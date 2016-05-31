5f73e0572894d4a7133e349f72055c66762cfddc
gs20060101 <genshib+2006@gmail.com>
2016-05-31 10:22:04 -0700

    Remove unnecessary arguments
    The function "fn" is a user-supplied function that would not take "index" and "arr" arguments.

--- exercises/implement_map_with_reduce/solution/solution.js
+++ exercises/implement_map_with_reduce/solution/solution.js
@@ -1,6 +1,6 @@
 module.exports = function arrayMap(arr, fn, thisArg) {
-  return arr.reduce(function(acc, item, index, arr) {
-    acc.push(fn.call(thisArg, item, index, arr))
+  return arr.reduce(function(acc, item) {
+    acc.push(fn.call(thisArg, item))
     return acc
   }, [])
 }
