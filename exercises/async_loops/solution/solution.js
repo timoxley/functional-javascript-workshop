cec5a93994366cf883f5123c4312dd671a1d78a3
Daniel Levy <justsml@users.noreply.github.com>
2017-05-10 22:01:17 -0600

    Update solution.js

--- exercises/async_loops/solution/solution.js
+++ exercises/async_loops/solution/solution.js
@@ -1,7 +1,8 @@
 const Promise = require('bluebird');
 
-module.exports = function loadUsers(userIds, load) {
+module.exports = function loadUsers(userIds, load, done) {
   load = Promise.promisify(load); // could be eliminated
   return Promise.resolve(userIds)
-    .map(id => load(id));
+    .map(id => load(id))
+    .then(done);
 }
