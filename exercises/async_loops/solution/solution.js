80bd33e4bb613fda6ddaa8f7a82c881c09a7a2a5
Daniel Levy <justsml@users.noreply.github.com>
2017-05-10 21:59:53 -0600

    Tightened code up
    Now 6 lines.

--- exercises/async_loops/solution/solution.js
+++ exercises/async_loops/solution/solution.js
@@ -1,11 +1,7 @@
 const Promise = require('bluebird');
-/* 
-Example using `Functional Promises` - removes side-effects, extra logical branching and mutable array usage.
-*/
+
 module.exports = function loadUsers(userIds, load) {
-  load = Promise.promisify(load);
-  return Promise
-    .resolve(userIds)
+  load = Promise.promisify(load); // could be eliminated
+  return Promise.resolve(userIds)
     .map(id => load(id));
 }
-
