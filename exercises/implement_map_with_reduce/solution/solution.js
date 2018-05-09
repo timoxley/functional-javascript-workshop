cb61d7d04183524d0a96b1777e8d08b5b3e5f649
Aonghus O Nia <aonghusonia@eircom.net>
2018-05-08 23:38:50 -0400

    modified to work with sparse arrays

--- exercises/implement_map_with_reduce/solution/solution.js
+++ exercises/implement_map_with_reduce/solution/solution.js
@@ -1,6 +1,6 @@
 module.exports = function arrayMap(arr, fn, thisArg) {
   return arr.reduce(function(acc, item, index, arr) {
-    acc.push(fn.call(thisArg, item, index, arr))
+    acc[index] = (fn.call(thisArg, item, index, arr))
     return acc
   }, [])
 }
